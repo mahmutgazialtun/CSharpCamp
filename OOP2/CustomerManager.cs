e3595d01d76a9b752177fdaa808d17e07d7b558f
mahmutgazialtun <65680877+mahmutgazialtun@users.noreply.github.com>
2022-03-17 16:05:42 +0300

    5.gün sonu

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //business classes
+    
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //iş kuralları yazılır.
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Gazi";
+            musteri1.Soyadi = "Altun";
+            musteri1.TcNo = "1231231313";
+
+
+            //Tüzel müşteri
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "4242424";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "123123133221";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManeger = new CustomerManager();
+            customerManeger.Add(musteri1);
+            customerManeger.Add(musteri2);
+
+        }
+    }
+}
